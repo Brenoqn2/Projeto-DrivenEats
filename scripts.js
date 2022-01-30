1f6d9d075760c0d30d73574776f998116ebc34d1
Breno Silva <brenoqn2@gmail.com>
2022-01-29 22:31:09 -0300

    Versão final

--- scripts.js
+++ scripts.js
@@ -1,89 +1,66 @@
-var countPrato = 0;
-var countBebida = 0;
-var countSobremesa = 0;
-var countPedido = 0;
+let countPrato = 0;
+let countBebida = 0;
+let countSobremesa = 0;
+let countPedido = 0;
 
-var pratoEscolhido ='';
-var bebidaEscolhida = '';
-var sobremesaEscolhida = '';
+let pratoEscolhido ='';
+let bebidaEscolhida = '';
+let sobremesaEscolhida = '';
 
-var pratoFinal ='';
+let pratoFinal ='';
 
-function selecionarPrato(clicked_id){
-    tirarSelecionadoPrato();
-    document.getElementById(clicked_id).style.boxShadow = "0px 0px 0px 5px #32B72F";
-    var check = document.createElement("ion-icon")
+function selecionar(elemento){
+    if (elemento.classList.contains("prato")){
+        countPrato ++;
+        pratoEscolhido=String(elemento.id);
+        let opcoes = document.getElementById("opcoesPratos");
+        let opcao = opcoes.getElementsByClassName("opcao");   
+        for (let i = 0; i < opcao.length; i++){
+            opcao[i].style.boxShadow = "0px 4px 4px rgba(0, 0, 0, 0.25)";
+            let check = opcao[i].getElementsByClassName("checkmark");
+            try{
+                check[0].remove()
+            }
+            catch(e){};
+        }  
+        }
+
+    if (elemento.classList.contains("bebida")){
+        countBebida ++;
+        bebidaEscolhida=String(elemento.id);
+        let opcoes = document.getElementById("opcoesBebidas");
+        let opcao = opcoes.getElementsByClassName("opcao"); 
+        for (let i = 0; i < opcao.length; i++){
+            opcao[i].style.boxShadow = "0px 4px 4px rgba(0, 0, 0, 0.25)";
+            let check = opcao[i].getElementsByClassName("checkmark");
+            try{
+                check[0].remove()
+            }
+            catch(e){};
+        }
+        }
+
+    if (elemento.classList.contains("sobremesa")){
+        countSobremesa ++;
+        sobremesaEscolhida=String(elemento.id);
+        let opcoes = document.getElementById("opcoesSobremesas");
+        let opcao = opcoes.getElementsByClassName("opcao");
+        for (let i = 0; i < opcao.length; i++){
+            opcao[i].style.boxShadow = "0px 4px 4px rgba(0, 0, 0, 0.25)";
+            let check = opcao[i].getElementsByClassName("checkmark");
+            try{
+                check[0].remove()
+            }
+            catch(e){};
+        }
+        }
+
+    elemento.style.boxShadow = "0px 0px 0px 5px #32B72F";
+    let check = document.createElement("ion-icon")
     check.setAttribute("name","checkmark-circle")
-    check.setAttribute("id","checkmarkid")
     check.setAttribute("class","checkmark")
-    document.getElementById(clicked_id).appendChild(check);
-    countPrato ++;
-    pratoEscolhido=clicked_id;
-    finalizarPedido();
-}
+    elemento.appendChild(check);    
 
-function selecionarBebida(clicked_id){
-    tirarSelecionadoBebida();
-    document.getElementById(clicked_id).style.boxShadow = "0px 0px 0px 5px #32B72F";
-    var check = document.createElement("ion-icon")
-    check.setAttribute("name","checkmark-circle")
-    check.setAttribute("id","checkmarkid")
-    check.setAttribute("class","checkmark")
-    document.getElementById(clicked_id).appendChild(check);
-    countBebida++;
-    bebidaEscolhida=clicked_id;
-    finalizarPedido();
-}
-
-function selecionarSobremesa(clicked_id){
-    tirarSelecionadoSobremesa();
-    document.getElementById(clicked_id).style.boxShadow = "0px 0px 0px 5px #32B72F";
-    var check = document.createElement("ion-icon")
-    check.setAttribute("name","checkmark-circle")
-    check.setAttribute("id","checkmarkid")
-    check.setAttribute("class","checkmark")
-    document.getElementById(clicked_id).appendChild(check);
-    countSobremesa++;
-    sobremesaEscolhida=clicked_id;
-    finalizarPedido();
-}
-
-function tirarSelecionadoPrato(){
-    var opcoes = document.getElementById("opcoesPratos");
-    var opcao = opcoes.getElementsByClassName("opcao");
-    for (let i = 0; i < opcao.length; i++){
-        opcao[i].style.boxShadow = "0px 4px 4px rgba(0, 0, 0, 0.25)";
-    }
-    tirarCheckMark();
-}
-
-function tirarSelecionadoBebida(){
-    var opcoes = document.getElementById("opcoesBebidas");
-    var opcao = opcoes.getElementsByClassName("opcao");
-    for (let i = 0; i < opcao.length; i++){
-        opcao[i].style.boxShadow = "0px 4px 4px rgba(0, 0, 0, 0.25)";
-    }
-    tirarCheckMark();
-}
-
-function tirarSelecionadoSobremesa(){
-    var opcoes = document.getElementById("opcoesSobremesas");
-    var opcao = opcoes.getElementsByClassName("opcao");
-    for (let i = 0; i < opcao.length; i++){
-        opcao[i].style.boxShadow = "0px 4px 4px rgba(0, 0, 0, 0.25)";
-    }
-    tirarCheckMark();
-}
-
-function tirarCheckMark(){
-    var check_Mark = document.getElementById("checkmarkid");
-    try {
-        check_Mark.remove();
-    } 
-    catch (e){};
-}
-
-function finalizarPedido(){
     if (countPrato >= 1 && countBebida >=1 && countSobremesa >=1 && countPedido == 0) {
         let botaoInferior = document.getElementById("botaoInferior");
         let texto = document.getElementById("textoInicial");
@@ -102,9 +79,10 @@ function fecharPedido(){
     if (countPedido == 0){}
     
     else{
-        var precoBebida = 'preço' + bebidaEscolhida; 
-        var precoPrato = 'preço' + pratoEscolhido;
-        var precoSobremesa = 'preço' + sobremesaEscolhida;
+        let precoBebida = 'preço' + bebidaEscolhida; 
+        let precoPrato = 'preço' + pratoEscolhido;
+        let precoSobremesa = 'preço' + sobremesaEscolhida;
+        
         precoBebida = document.getElementById(precoBebida).textContent;
         precoBebida = precoBebida.split(" ").pop();
         precoBebida =  parseFloat(precoBebida.replace(',','.'));
@@ -117,24 +95,24 @@ function fecharPedido(){
         precoSobremesa = precoSobremesa.split(" ").pop();
         precoSobremesa =  parseFloat(precoSobremesa.replace(',','.'));
 
-        var precoFinal =  precoBebida + precoPrato + precoSobremesa;
+        let precoFinal =  precoBebida + precoPrato + precoSobremesa;
         precoFinal = precoFinal.toFixed(2);
         precoBebida = precoBebida.toFixed(2);
         precoPrato = precoPrato.toFixed(2);
         precoSobremesa = precoSobremesa.toFixed(2);
 
          
-        var bebidaFinal = 'nome' + bebidaEscolhida;
+        let bebidaFinal = 'nome' + bebidaEscolhida;
         bebidaFinal = document.getElementById(bebidaFinal).textContent;
-        var pratoFinal = 'nome' + pratoEscolhido;
+        let pratoFinal = 'nome' + pratoEscolhido;
         pratoFinal = document.getElementById(pratoFinal).textContent;
-        var sobremesaFinal = 'nome' + sobremesaEscolhida;
+        let sobremesaFinal = 'nome' + sobremesaEscolhida;
         sobremesaFinal = document.getElementById(sobremesaFinal).textContent;
 
-        var mensagemPronta =  "Olá, gostaria de fazer o pedido:\n- Prato: "+ pratoFinal + "\n- Bebida: " + bebidaFinal + "\n- Sobremesa: "+ sobremesaFinal + "\nTotal: R$ " + precoFinal;
+        let mensagemPronta =  "Olá, gostaria de fazer o pedido:\n- Prato: "+ pratoFinal + "\n- Bebida: " + bebidaFinal + "\n- Sobremesa: "+ sobremesaFinal + "\nTotal: R$ " + precoFinal;
 
-        var nomeCliente = prompt("Qual o seu nome?");
-        var enderecoCliente = prompt("Qual seu endereço?");
+        let nomeCliente = prompt("Qual o seu nome?");
+        let enderecoCliente = prompt("Qual seu endereço?");
 
         mensagemPronta = mensagemPronta + "\n \nNome: " + nomeCliente +"\nEndereço: " + enderecoCliente 
 
@@ -147,13 +125,13 @@ function fecharPedido(){
         linkBotao = document.getElementById("linkWhatsapp");
         linkBotao.setAttribute("href",link);
 
-        document.getElementById("nomePratoConfirma").innerHTML = pratoFinal
-        document.getElementById("precoPratoConfirma").innerHTML = precoPrato
-        document.getElementById("nomeBebidaConfirma").innerHTML = bebidaFinal
-        document.getElementById("precoBebidaConfirma").innerHTML = precoBebida
+        document.getElementById("nomePratoConfirma").innerHTML = pratoFinal;
+        document.getElementById("precoPratoConfirma").innerHTML = precoPrato.replace('.',',');
+        document.getElementById("nomeBebidaConfirma").innerHTML = bebidaFinal;
+        document.getElementById("precoBebidaConfirma").innerHTML = precoBebida.replace('.',',');
         document.getElementById("nomeSobremesaConfirma").innerHTML = sobremesaFinal;
-        document.getElementById("precoSobremesaConfirma").innerHTML = precoSobremesa;
-        document.getElementById("precoTotal").innerHTML = "R$ " + precoFinal;
+        document.getElementById("precoSobremesaConfirma").innerHTML = precoSobremesa.replace('.',',');
+        document.getElementById("precoTotal").innerHTML = "R$ " + precoFinal.replace('.',',');
     
         let  div = document.getElementById("confirmarDados");
         div.setAttribute("class","on");
